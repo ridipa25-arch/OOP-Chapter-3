81af92a310fabd3ee041cb007e264bb71ddef54d
ridipa25-arch <ri.dipa25@ncmt.edu.np>
2026-09-02 18:02:36 +0545

    Add files via upload

--- chapter-5/main.py
+++ chapter-5/main.py
@@ -1,9 +1,10 @@
-import mathops
-print("Addition:",mathops.add(10,2))
-print("Subtraction:",mathops.subtract(10,2))
-from mathops import add,subtract
-print("Addition:",mathops.add(20,10))
-print("Subtraction:",mathops.subtract(20,10))
-import mathops as m
-print("Addition:",m.add(30,50))
-print("Subtraction:",m.subtract(30,50))
+import csv
+
+with open("students.csv", "r", newline="") as file:
+    reader = csv.DictReader(file)
+
+    for student in reader:
+        print("Roll No:", student["Roll no"])
+        print("Name:", student["Name"])
+        print("Address:", student["Address"])
+        print()
